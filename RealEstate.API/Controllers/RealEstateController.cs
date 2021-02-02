4b3b0df898fef8e4f0519e0dc455bb09dfaa38bc
Kisielos10 <Kisielos101@gmail.com>
2021-02-02 20:14:41 +0100

    Initial

---
+++ RealEstate.API/Controllers/RealEstateController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RealEstate.API.DTO;
+
+namespace RealEstate.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RealEstateController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<RealEstateDto> Get()
+        {
+            List<RealEstateDto> realEstateDtos = new List<RealEstateDto>
+            {
+                new RealEstateDto()
+                {
+                    Area = 200.2m
+                }
+            };
+            return realEstateDtos;
+        }
+    }
+}
---
+++ RealEstate.API/DTO/RealEstateDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+//TODO rozwinąć RealEstateController i RealEstateDto(Id) sensowne atrybuty i property w kontrolerze get by id 
+//TODO całkiem nowy controller na notatki związane z real estatem i do tego nowy DTO
+namespace RealEstate.API.DTO
+{
+    /// <summary>
+    /// cokolwiek
+    /// </summary>
+    public class RealEstateDto
+    {
+        [Range(0,20000000)]
+        public decimal Price { get; set; }
+        public decimal Area { get; set; }
+        public AddressDto Address { get; set; }
+    }
+
+    public class AddressDto
+    {
+        public string PostalCode { get; set; }
+        public string StreetName { get; set; }
+        public int BuildingNumber { get; set; }
+        public int? ApartmentNumber { get; set; }
+    }
+}
