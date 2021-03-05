bd5e7c123693b137831be2f3cb1f39195fda0c03
Kisielos10 <Kisielos101@gmail.com>
2021-03-05 19:36:59 +0100

    TestDataGenerator.cs

---
+++ RealEstate.UnitTest/TestDataGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using RealEstate.API.DTO;
+
+namespace RealEstate.UnitTest
+{
+    class TestDataGenerator : IEnumerable<object[]>
+    {
+        public static IEnumerable<object[]> GetRealEstateFromDataGenerator()
+        {
+            yield return new object[]
+            {
+                new List<RealEstateDto>
+                {
+                    new RealEstateDto
+                    {
+                        Id = 0,
+                        Area = 200m,
+                        Price = 5000000m
+                    },
+                    new RealEstateDto
+                    {
+                        Id = 1,
+                        Area = 300m,
+                        Price = 90000000m
+                    }
+                }
+            };
+        }
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
